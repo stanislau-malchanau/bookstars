f70f24b2f85ca7186f57f8590af7c2caf835dd83
stanislau-malchanau <s.malchanau@7tam.net>
2025-09-07 20:15:36 +0300

    added media files settings

--- bookstars/urls.py
+++ bookstars/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -25,3 +27,6 @@ urlpatterns = [
     path('books/', include('books.urls')),
     path('', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
