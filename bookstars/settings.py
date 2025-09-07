8e932b242543b14c9eba9bd938b91217626e2484
stanislau-malchanau <s.malchanau@7tam.net>
2025-09-07 18:21:16 +0300

    added templates, forms, urls, views (log in & sign up process)

--- bookstars/settings.py
+++ bookstars/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -57,10 +59,11 @@ ROOT_URLCONF = 'bookstars.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Указываем путь к общей папке templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -68,6 +71,9 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGIN_REDIRECT_URL = '/'  # Куда перенаправлять после логина
+LOGOUT_REDIRECT_URL = '/' # Куда перенаправлять после выхода
 
 WSGI_APPLICATION = 'bookstars.wsgi.application'
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Автоматический логин после регистрации
+            return redirect('core:home')  # Перенаправление на главную (пока заглушка)
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
